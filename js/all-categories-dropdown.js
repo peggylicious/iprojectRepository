3f5bea18a198aa23e5d056c3c479f2582f198040
peggylicious <p.asq@live.com>
2019-10-09 14:52:39 +0100

    Add frontend all categories and change folder structure

---
+++ js/all-categories-dropdown.js
@@ -0,0 +1,17 @@
+// $('ul.nav li.dropdown').mouseenter(function() {
+//     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+//   }, function() {
+//     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+//   });
+// $(".dropdown-toggle").on("mouseenter", function () {
+//     // make sure it is not shown:
+//     if (!$(this).parent().hasClass("show")) {
+//         $(this).click();
+//     }
+// });
+// $(".btn-group, .dropdown").on("mouseleave", function () {
+//     // make sure it is shown:
+//     if ($(this).hasClass("show")){
+//         $(this).children('.dropdown-toggle').first().click();
+//     }
+// });
