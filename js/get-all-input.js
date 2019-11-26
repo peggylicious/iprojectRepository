f0b882c875276308707f5504cbca0f39952fd9b3
peggylicious <p.asq@live.com>
2019-11-26 15:23:16 +0100

    Add home page

--- js/get-all-input.js
+++ js/get-all-input.js
@@ -1,28 +1,34 @@
-// Create dictionary to store all input value
+// Create dictionary to store all input values
 let input_data = {};
 // Get element by attribute on button click
 document.addEventListener('click', function () {
+    // Get all inputs
+    let getInputElements = document.querySelectorAll("#my-form .form-control");
     if(event.target.matches("form button")){
         // Prevent button reload
         event.preventDefault();
-        // get parent of clicked element and assign id
-        event.target.parentElement.id = "hello";
 
-        // Get children of parent with a particular class
-        // get all-inputs = document.querySelector()
-        let getInputElements = document.querySelectorAll("#hello .form-control");
-        // Loop through children
+
+        // Loop through inputs
         for(i = 0; i < getInputElements.length; i++){
-            // Check if inputElements has a 'name' attribute
-            if(getInputElements[i].hasAttribute("name")){
-                // Get each attribute value
-                inputAtt = getInputElements[i].getAttribute("name");
-                // Get each value user entered and assign it to dictionary key/value pairs
-                input_data[inputAtt] = getInputElements[i].value;
+            // Get each input item value
+            let inputElValue = getInputElements[i].value;
+            // Get name attribute value
+            let inputAtt = getInputElements[i].getAttribute("name");
+            // Check if input value is empty
+            if(inputElValue !== "" ){
+                // Check if input element has name attribute
+                // And assign input value and attribute value as key value pairs to dictionary
+                if(getInputElements[i].hasAttribute("name")){
+                    input_data[inputAtt] = inputElValue;
+                }
+            }else{
+                alert("Some fields have not been filled e.g " + getInputElements[i].getAttribute("name"));
+                return false;
             }
         }
+        // log result in console
+        console.log(input_data);
     }
-    console.log(input_data);
+
 }, false)
-//
-//
